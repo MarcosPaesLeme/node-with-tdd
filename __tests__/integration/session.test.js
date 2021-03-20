9ff9408af32976271b466d37c945336cbd67b60b
Marcos Paes Leme de Souza <Marcos.Paes.Leme.De.Souza@ibm.com>
2021-03-20 20:50:47 -0300

    test(sessions.test.js): create test to check with will return in the response

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -37,4 +37,19 @@ describe('Authentication', () => {
 
     expect(response.status).toBe(401);
   });
+
+  it('should return jwt token when authenticated', async () => {
+    const user = await User.create({
+      name: 'Marcos',
+      email: 'marcos2@gmail.com',
+      password: '123123',
+    });
+
+    const response = await request(app).post('/sessions').send({
+      email: user.email,
+      password: '123123',
+    });
+
+    expect(response.body).toHaveProperty('token');
+  });
 });
